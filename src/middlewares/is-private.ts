99ba3ba3cacba904359e633c4cc10752d3d58bd0
Ali Hashemi <mr.ali.haashemi@gmail.com>
2025-10-02 00:27:43 +0330

    fix: fix checking the wrong field

--- src/middlewares/is-private.ts
+++ src/middlewares/is-private.ts
@@ -1,7 +1,7 @@
 import type { Context, NextFunction } from "grammy";
 
 export const isPrivate = async (ctx: Context, next: NextFunction): Promise<void> => {
-  const isInPrivate = ctx.chatId === ctx.senderChat?.id;
+  const isInPrivate = ctx.chat?.type === "private";
 
   if (isInPrivate) await next();
 };
