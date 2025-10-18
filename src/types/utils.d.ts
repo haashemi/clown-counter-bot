4e78b8283a335e6961e622dffd0e45d3a44d0596
Ali Hashemi <mr.ali.haashemi@gmail.com>
2025-10-18 11:58:47 +0330

    chore: update deps and fix linting errors

--- src/types/utils.d.ts
+++ src/types/utils.d.ts
@@ -2,5 +2,5 @@
  * DeepPartial makes all fields from the type T optional.
  */
 type DeepPartial<T> = {
-  [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
+  [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P] | undefined;
 };
