dd37a4b8258e88948326e5adc39b30511eb5ff8b
Ali Hashemi <mr.ali.haashemi@gmail.com>
2026-07-25 04:09:01 +0330

    fix: corece the env numders

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -9,7 +9,7 @@ export const config = createEnv({
       .string()
       .length(46)
       .regex(/^\d{10}:.+/),
-    BOT_SUPERUSER: z.number(),
+    BOT_SUPERUSER: z.coerce.number(),
     /** Database (SQLite) file path */
     DB_FILE_PATH: z.string().startsWith("file:"),
   },
