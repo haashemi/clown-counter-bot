9c883a1e5115072095e5a0e7e3d28f51695f237c
Ali Hashemi <mr.ali.haashemi@gmail.com>
2026-07-25 04:04:49 +0330

    feat: add bot runner

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
+import { run } from "@grammyjs/runner";
+
 import { Bot } from "@/lib/bot";
 import { config } from "@/lib/config";
 
 import { commands } from "./commands";
-import { clownHandler } from "./commands/clown";
+import { clownHandler, isClownCall } from "./commands/clown";
 
-const bot = new Bot(config.botToken);
-
-await commands.setCommands(bot);
+const bot = new Bot(config.BOT_TOKEN);
 
 bot.use(commands);
 
-bot.filter((ctx) => ["group", "supergroup"].includes(ctx.chat?.type ?? "")).hears(["🤡", "دلقک"], clownHandler);
+bot
+  .filter((ctx) => !!ctx.chat && ["group", "supergroup"].includes(ctx.chat.type))
+  .on("message", isClownCall, clownHandler);
 
-bot.start();
+await commands.setCommands(bot);
+
+run(bot, {
+  runner: { fetch: { allowed_updates: ["message"] } },
+});
